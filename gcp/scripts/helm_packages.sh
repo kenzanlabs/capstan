ce61259303728e3ccf3f55bdd284a98dc48750c5
Nicholas J. Parks <nparks@kenzan.com>
2018-05-22 15:26:46 -0400

    more about helm and less about jenkins

--- gcp/scripts/helm_packages.sh
+++ gcp/scripts/helm_packages.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 ##########################
-# Kenzan LLC -> Jenkins on Existing GKE
-# 
-# Can your GCP Service Account do this?
-# Did you create the GKE environment first?
-#
-# nparks@kenzan.com
+# Kenzan LLC -> Helm packages GKE
 ##########################
 source $PWD/env.sh
 
@@ -16,10 +11,12 @@ echo " - Let's Get this Jenkins Thing Together -"
 echo "=========================================="
 
 #expecting tiller is in the environment
+
+##nginx Ingress
+helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true
+## Jenkins
 #the wait flag means everything should be done...the replace flag means stop over existing
 helm install stable/jenkins --name $JENKINS_HELM_RELEASENAME --namespace $JENKINSNS --replace --wait
-
-
 JENKINS_ADDRESS=$(kubectl get svc --namespace jenkins ci-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
 JENKINS_PW=`printf $(kubectl get secret --namespace $JENKINS_NAMESPACE $JENKINS_HELM_RELEASENAME-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo`
 
