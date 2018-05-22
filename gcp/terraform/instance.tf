ab310b839796bb19650b69f5213cda75b4c7d524
Nicholas J. Parks <nparks@kenzan.com>
2018-05-22 15:23:42 -0400

    change to reference that we are doing helm packages not just jenkins

--- gcp/terraform/instance.tf
+++ gcp/terraform/instance.tf
@@ -1,54 +1,56 @@
 resource "google_compute_instance" "halyardtunnel" {
-    name = "${var.halyard_machine_name}"
-    machine_type = "${var.halyard_machine_type}"
-    zone = "${var.region}-${var.zone}"
+  name         = "${var.halyard_machine_name}"
+  machine_type = "${var.halyard_machine_type}"
+  zone         = "${var.region}-${var.zone}"
 
-    depends_on = [
-        #"google_project_service.iam_service",
-        #"google_project_service.cloudresourcemanager_service",
-        #"google_project_service.container_service",
-        "google_project_iam_policy.project",
-        "google_container_cluster.primary"
+  depends_on = [
+    #"google_project_service.iam_service",
+    #"google_project_service.cloudresourcemanager_service",
+    #"google_project_service.container_service",
+    "google_project_iam_policy.project",
+
+    "google_container_cluster.primary",
+  ]
+
+  tags = ["halyard", "${var.created_by}", "ssh-tunnel"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.ubuntu_image}"
+    }
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "cloud-platform"]
+    email  = "${google_service_account.halyard_toolsacct.email}"
+  }
+
+  connection {
+    user        = "${var.ssh_user}"
+    private_key = "${file(var.ssh_private_key_location)}"
+    agent       = false
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  provisioner "file" {
+    source      = "../scripts/"
+    destination = "/home/${var.ssh_user}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/${var.ssh_user}/*.sh",
+      "/home/${var.ssh_user}/instance_execute.sh",
+      "/home/${var.ssh_user}/create_GKE.sh ${var.gcp_project_id} ${var.gke_cluster_name} ${var.gke_primary_zone}",
+      "/home/${var.ssh_user}/helm_packages.sh",
+      "/home/${var.ssh_user}/halyard_createSpinnaker.sh ${var.gcp_project_id} ${var.gke_cluster_name} ${var.gke_primary_zone}",
     ]
-
-    tags = ["halyard","${var.created_by}","ssh-tunnel"]
-
-    boot_disk {
-        initialize_params {
-            image = "${var.ubuntu_image}"
-        }
-    }
-
-    service_account {
-        scopes = ["userinfo-email", "cloud-platform"]
-        email="${google_service_account.halyard_toolsacct.email}"
-    }
-
-    connection {
-        user = "${var.ssh_user}"
-        private_key = "${file(var.ssh_private_key_location)}"
-        agent = false
-    }
-
-    network_interface {
-        network = "default"
-        access_config {
-            // Ephemeral IP
-        }
-    }
-
-    provisioner "file" {
-        source = "../scripts/"
-        destination = "/home/${var.ssh_user}"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "chmod +x /home/${var.ssh_user}/*.sh",
-            "/home/${var.ssh_user}/instance_execute.sh",
-            "/home/${var.ssh_user}/create_GKE.sh ${var.gcp_project_id} ${var.gke_cluster_name} ${var.gke_primary_zone}",
-            "/home/${var.ssh_user}/create_jenkins.sh",
-            "/home/${var.ssh_user}/halyard_createSpinnaker.sh ${var.gcp_project_id} ${var.gke_cluster_name} ${var.gke_primary_zone}"
-        ]
-    }
+  }
 }
