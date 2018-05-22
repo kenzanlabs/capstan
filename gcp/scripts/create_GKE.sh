3950971546295e86c577a28b04c7d6449de4ccc6
Nicholas J. Parks <nparks@kenzan.com>
2018-05-22 15:20:06 -0400

    remove redundant api calls because they are executed as part of project verifications

--- gcp/scripts/create_GKE.sh
+++ gcp/scripts/create_GKE.sh
@@ -23,13 +23,6 @@ echo "  CLUSTER_ZONE: $CLUSTER_ZONE"
 # enable commands
 echo ">>>>> Enable gcloud API needs"
 
-#we should not need these API calls because
-# 1. use services instead of service-management
-# 2. Initial project verification requires you to execute these api calls already
-gcloud service-management enable iam.googleapis.com
-gcloud service-management enable cloudresourcemanager.googleapis.com
-gcloud service-management enable container.googleapis.com
-
 #make kubectl happy for later usage
 gcloud config set container/use_client_certificate true
 gcloud container clusters get-credentials --zone $CLUSTER_ZONE $CLUSTER_NAME
@@ -41,7 +34,6 @@ kubectl create -f tiller_rbac.yml
 sleep 5
 helm init --service-account tiller --wait
 # give the cluster a break, if your K8 is small
-sleep 5
 helm version
 
 echo "=========================================="
