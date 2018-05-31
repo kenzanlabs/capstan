d151d870bc7b1c1249109aebae497066d19baad6
Nicholas J. Parks <nparks@kenzan.com>
2018-05-31 12:15:05 -0400

    add a listing of packages to enforce the concept that we doing a package install step and not just a jenkins step

--- gcp/scripts/helm_packages.sh
+++ gcp/scripts/helm_packages.sh
@@ -7,14 +7,25 @@ source $PWD/env.sh
 
 echo "******************************************"
 echo "=========================================="
-echo " - Let's Get this Jenkins Thing Together -"
+echo " - Let's Get Some K8 Packages Together -"
 echo "=========================================="
 
 #expecting tiller is in the environment
+################## Optional Charts
+###nginx Ingress
+#helm install stable/nginx-ingress --name nginx-ingress --namespace nginxingress --set rbac.create=true
 
-##nginx Ingress
-helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true
-## Jenkins
+### Istio with sidecar injection Requires K8 1.9
+#helm install install/kubernetes/helm/istio --name istio --namespace istio-system
+
+### Cert-Manager
+#helm install stable/cert-manage --name cert-mngr --namespace certmngr --set rbac.create=true
+
+### DNS because if you have cert manager you might want the DNS help also
+#helm install stable/external-dns --name dns --namespace dns -f external-dns.values
+
+################ Required Charts
+### Jenkins
 #the wait flag means everything should be done...the replace flag means stop over existing
 helm install stable/jenkins --name $JENKINS_HELM_RELEASENAME --namespace $JENKINSNS --replace --wait
 JENKINS_ADDRESS=$(kubectl get svc --namespace jenkins ci-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
