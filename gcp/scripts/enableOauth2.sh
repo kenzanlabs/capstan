ff00efe85f637628024f1aa507f38f78082baf5b
Nicholas J. Parks <nparks@kenzan.com>
2018-06-04 15:49:35 -0400

    add the oauth2

---
+++ gcp/scripts/enableOauth2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+##########################
+# Configure Oauth2
+##########################
+###
+source $PWD/env.sh
+
+echo "******************************************"
+echo "=========================================="
+echo " - Let's get Oauth2 Config Together -"
+echo "=========================================="
+
+hal config security ui edit --override-base-url $DNSTLSUI
+hal config security api edit --override-base-url $DNSTLSAPI
+hal config security authn oauth2 edit --client-id $CLIENT_ID  --client-secret $CLIENT_SECRET --provider $PROVIDER  --user-info-requirements hd=$GSDOMAIN
+hal config security authn oauth2 enable
+
+
+echo "=========================================="
+echo " - Core Spinnaker Config -"
+echo "=========================================="
+echo "******************************************"
