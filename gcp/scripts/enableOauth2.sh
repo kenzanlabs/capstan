972283362c014a40cec3e2a62603769b081c443e
Nicholas J. Parks <nparks@kenzan.com>
2018-06-11 09:42:21 -0400

    parameters for OAUTH2 enablement

--- gcp/scripts/enableOauth2.sh
+++ gcp/scripts/enableOauth2.sh
@@ -11,13 +11,20 @@ echo "=========================================="
 echo " - Let's get Oauth2 Config Together -"
 echo "=========================================="
 
-hal config security ui edit --override-base-url $DNSTLSUI
-hal config security api edit --override-base-url $DNSTLSAPI
-hal config security authn oauth2 edit --client-id $CLIENT_ID  --client-secret $CLIENT_SECRET --provider $PROVIDER  --user-info-requirements hd=$GSDOMAIN
+PROJECT_NAME=$1
+DNSTLSUI=$2
+DNSTLSAPI=$3
+CLIENT_ID=$4
+CLIENT_SECRET=$5
+GSDOMAIN=$6
+
+hal config security ui edit --override-base-url "https://$DNSTLSUI"
+hal config security api edit --override-base-url "https://$DNSTLSAPI"
+hal config security authn oauth2 edit --client-id $CLIENT_ID  --client-secret $CLIENT_SECRET --provider google --user-info-requirements hd=$GSDOMAIN
 hal config security authn oauth2 enable
 
 
 echo "=========================================="
-echo " - OAUTH2 config -"
+echo " - OAUTH2 config complete -"
 echo "=========================================="
 echo "******************************************"
