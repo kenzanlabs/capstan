cf45a54e8548feca73377d9ea650c210d089086c
Nicholas J. Parks <nparks@kenzan.com>
2018-09-17 14:54:43 -0400

    ignore another generated file that we generate and make the network proper

--- aws/terraform/tools.tf
+++ aws/terraform/tools.tf
@@ -2,8 +2,12 @@
 # tools instance to run bash/ansible
 #
 
-##### We need the kubeconf file
+##### We need the kubeconf an authenticator yaml file
 
+resource "local_file" "configmap" {
+    content     = "${local.config_map_aws_auth}"
+    filename = "generated-configmap.yaml"
+}
 
 resource "local_file" "kubeconf" {
     content     = "${local.kubeconfig}"
--- aws/terraform/vpc.tf
+++ aws/terraform/vpc.tf
@@ -14,6 +14,25 @@ resource "aws_vpc" "containernet" {
   }"
 }
 
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${aws_vpc.containernet.id}"
+
+  tags {
+    Name = "${var.net_name}"
+  }
+}
+
+
+resource "aws_route_table" "rt" {
+  vpc_id = "${aws_vpc.containernet.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ig.id}"
+  }
+}
+
+
 
 resource "aws_subnet" "sbnet" {
   count = 2
@@ -28,4 +47,11 @@ resource "aws_subnet" "sbnet" {
      "kubernetes.io/cluster/${var.gen_solution_name}", "shared",
     )
   }"
+}
+
+resource "aws_route_table_association" "rta" {
+  count = 2
+
+  subnet_id      = "${aws_subnet.sbnet.*.id[count.index]}"
+  route_table_id = "${aws_route_table.rt.id}"
 }
