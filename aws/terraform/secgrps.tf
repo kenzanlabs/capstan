1806ca7609e8af76ac615597d1a3ceef633987df
Nicholas J. Parks <nparks@kenzan.com>
2018-09-26 10:55:35 -0400

    security groups for the bastion

--- aws/terraform/secgrps.tf
+++ aws/terraform/secgrps.tf
@@ -31,6 +31,17 @@ resource "aws_security_group_rule" "cluster-ingress-node-https" {
   type                     = "ingress"
 }
 
+
+resource "aws_security_group_rule" "cluster-ingress-tools-https" {
+  description              = "Allow bastion to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.cluster.id}"
+  source_security_group_id = "${aws_security_group.bastion.id}"
+  to_port                  = 443
+  type                     = "ingress"
+}
+
 resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
   cidr_blocks       = ["${local.workstation-external-cidr}"]
   description       = "Allow workstation to communicate with the cluster API Server"
@@ -80,4 +91,39 @@ resource "aws_security_group_rule" "ingress-cluster" {
   source_security_group_id = "${aws_security_group.cluster.id}"
   to_port                  = 65535
   type                     = "ingress"
+}
+
+
+###### Bastion/Tunnel/Tools instance
+
+
+resource "aws_security_group" "bastion" {
+  name        = "${var.gen_solution_name}-tools"
+  description = "Security group for the tools-tunnel-bastion"
+  vpc_id      = "${aws_vpc.containernet.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${
+    map(
+     "Name", "${var.gen_solution_name}-tools",
+     "Usage", "kubernetes.io/cluster/${var.gen_solution_name}",
+    )
+  }"
+}
+
+
+resource "aws_security_group_rule" "tools-ingress-workstation-https" {
+  cidr_blocks       = ["${local.workstation-external-cidr}"]
+  description       = "Allow workstation to ssh into bastion"
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.bastion.id}"
+  to_port           = 22
+  type              = "ingress"
 }
