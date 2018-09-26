df0784eff2cf6ca9851aff3101236a88980cb4f4
Nicholas J. Parks <nparks@kenzan.com>
2018-09-26 12:36:07 -0400

    connection and subnet setting

--- aws/terraform/tools.tf
+++ aws/terraform/tools.tf
@@ -22,9 +22,26 @@ resource "aws_instance" "bastion" {
   key_name = "${var.ec2_key}"
   vpc_security_group_ids =["${aws_security_group.bastion.id}"]
   associate_public_ip_address = true
+  subnet_id = "${aws_subnet.sbnet.*.id[0]}"
   provisioner "file" {
     source      = "${local_file.kubeconf.filename}"
-    destination = "/home/${var.ec2_ssh_user}"
+    destination = "/home/${var.ec2_ssh_user}/"
+
+  connection {
+    user        = "${var.ec2_ssh_user}"
+    private_key = "${file(var.ec2_key_file)}"
+    agent       = false
   }
+
+  }
+
+    tags = "${
+    map(
+     "Name", "${var.gen_solution_name}-tools",
+     "Usage", "kubernetes.io/cluster/${var.gen_solution_name}",
+    )
+  }"
+
+  
   
 }
