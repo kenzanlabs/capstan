20ddf4960760b6e22c466a1d68780b0e98856dad
Nicholas J. Parks <nparks@kenzan.com>
2019-02-05 15:57:11 -0500

    ALB and kube2iam placeholder

---
+++ aws/scripts/02_pltreq_albIngress.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+######################
+# We need and ingress controller for the ALB
+# Depends on 
+######################
---
+++ aws/scripts/03_pltreq_kube2iam.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+######################
+# We need Kube2iam
+# Depends on Helm
+######################
