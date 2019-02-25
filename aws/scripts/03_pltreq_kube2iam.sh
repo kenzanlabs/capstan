dbafd6915cea17f08360ab0d9b9e1a0bfe4d797d
Nicholas J. Parks <nparks@kenzan.com>
2019-02-25 13:40:00 -0500

    we will use the HELM the chart to install kube2iam, let's try these options

--- aws/scripts/03_pltreq_kube2iam.sh
+++ aws/scripts/03_pltreq_kube2iam.sh
@@ -4,3 +4,20 @@
 # We need Kube2iam because we a proper users of AWS IAM controllers
 # Depends on Helm
 ######################
+
+
+echo "******************************************"
+echo "=========================================="
+echo " - BEGIN KUBE2IAM -"
+echo "=========================================="
+
+#not installing extra IAM roles
+helm install stable/kube2iam --name k2iam --set rbac.create=true,host.iptables=true,host.interface=eni+
+
+
+echo "=========================================="
+echo " - END KUBE2IAM -"
+echo "=========================================="
+echo "******************************************"
+#pause log dump
+sleep 10
